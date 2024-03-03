e26b68f11b266e1d23908fb221a3c8f62369b6ae
Karla Lockhart <kblockhart@gmail.com>
2024-03-03 15:42:09 -0800

    passed tests

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-class Person:
+#class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+#    pass
+
+#!/usr/bin/env python3
+
+class Person:
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
